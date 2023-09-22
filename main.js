898519d5526c2eee786b5d657b55235050bb5089
pardaboyevkarim <pardaboyevkarim9@gmail.com>
2023-09-22 17:54:00 +0500

    add

---
+++ main.js
@@ -0,0 +1,5 @@
+var elButton = document.querySelector(".site-header__buttontou");
+let elnav = document.querySelector("nav");
+elButton.addEventListener("click", function () {
+  elnav.classList.toggle("dark");
+});
